71db48da8d323339ddbd4fc410649eb22238f919
usermech <umutkrt98@gmail.com>
2023-12-25 15:57:40 +0300

    export functions

--- tiago-data-collection/export.py
+++ tiago-data-collection/export.py
@@ -4,47 +4,47 @@ import liblzfse
 import os
 from pathlib import Path
 
-data=np.load("data/log.npz")
+def save_rgb(img, episode_dir):
+    img=img.reshape(480,640,3,-1)    
+    #export img to vid
+    
+    out=cv2.VideoWriter(episode_dir / "/compressed_video_h264.mp4", cv2.VideoWriter_fourcc(*'mp4v'), 10, (img.shape[1], img.shape[0]))
+    for i in range(img.shape[-1]):
+        out.write(cv2.cvtColor(img[:,:,:,i], cv2.COLOR_BGR2RGB)) #fix colors and write to vid
+    out.release()
+
+def save_depth(depth, episode_dir):
+    depth=depth.reshape(480,640,1,-1)
+    #export depth to zipped binary
+    depth=depth.astype(np.float32)
+    depth/=2000 #normalize to max out at ~2 meters
+    depth=np.moveaxis(depth,-1,0)
+    depth=np.squeeze(depth, axis=-1)
+    print(depth.shape)
+    depth_bytes=liblzfse.compress(depth.tobytes())
+    (episode_dir / "compressed_np_depth_float32.bin").write_bytes(depth_bytes)
+
+def process_episode(filename, root_dir):
+    data=np.load(f'{filename}.npz')
+    img=data["img"]
+    depth=data["depth"]
+    episode_dir_name = f'dataset/task1/env1/{filename}' 
+    os.mkdir(episode_dir_name)
+    episode_dir = root_dir / episode_dir_name
+    save_all(img, depth, episode_dir)   # save rgb and depth images
+    os.rename(f'{filename}.json', episode_dir / 'labels.json')  # save action labels
 
 
-print(data.files)
+def save_all(img, depth, episode_dir):
+    save_rgb(img, episode_dir)
+    save_depth(depth, episode_dir)
 
-img=data["img"]
-depth=data["depth"]
-
-img=img.reshape(480,640,3,-1)
-print(img.shape)
-
-depth=depth.reshape(480,640,1,-1)
-
-#export img to vid
-out=cv2.VideoWriter("data/compressed_video_h264.mp4", cv2.VideoWriter_fourcc(*'mp4v'), 10, (img.shape[1], img.shape[0]))
-for i in range(img.shape[-1]):
-    out.write(cv2.cvtColor(img[:,:,:,i], cv2.COLOR_BGR2RGB)) #fix colors and write to vid
-out.release()
-
-#export depth to zipped binary
-depth=depth.astype(np.float32)
-depth/=2000 #normalize to max out at ~2 meters
-depth=np.moveaxis(depth,-1,0)
-depth=np.squeeze(depth, axis=-1)
-print(depth.shape)
-depth_bytes=liblzfse.compress(depth.tobytes())
+# read every .npz file in data/
+datafiles=[]
+for file in os.listdir("data"):
+    if file.endswith(".npz"):
+        datafiles.append(file.split(".")[0])
 
 root_dir=Path(os.getcwd())
-(root_dir / "compressed_np_depth_float32.bin").write_bytes(depth_bytes)
-
-
-# for i in range(img.shape[-1]):
-#     cv2.imshow("ha", img[:,:,:,i])
-#     while True:
-#         if cv2.waitKey(1) & 0xFF ==ord('q'):
-#             break
-#     cv2.destroyAllWindows()
-
-# for i in range(depth.shape[-1]):
-#     cv2.imshow("ha", depth[:,:,:,i])
-#     while True:
-#         if cv2.waitKey(1) & 0xFF ==ord('q'):
-#             break
-#     cv2.destroyAllWindows()
+for file in datafiles:    
+    process_episode(file)
