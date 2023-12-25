ceaf189e086b20b76df10029fdd2e63c8b275428
moneylobster <87999452+moneylobster@users.noreply.github.com>
2023-12-25 16:38:24 +0300

    view different stuff with input

--- tiago-data-collection/viewdepth.py
+++ tiago-data-collection/viewdepth.py
@@ -6,8 +6,9 @@ import numpy as np
 #import skimage.io as io
 import cv2
 
+epname=input("Give the episode name date thing pls: ")
 root_dir=Path(os.getcwd())
-depth_file=root_dir / "compressed_np_depth_float32.bin"
+depth_file=root_dir / f"dataset/task1/env1/{epname}/compressed_np_depth_float32.bin"
 raw_bytes=depth_file.read_bytes()
 decompressed_bytes = liblzfse.decompress(raw_bytes)
 depth_img = np.frombuffer(decompressed_bytes, dtype=np.float32)
@@ -15,7 +16,7 @@ depth_img = depth_img.reshape((-1, 480, 640))
 print(depth_img.shape)
 print(np.max(depth_img))
 #depth_img = np.ascontiguousarray(np.rot90(depth_img, -1))
-dimg=cv2.normalize(depth_img[85], None, 0,255, cv2.NORM_MINMAX)
+dimg=cv2.normalize(depth_img[len(depth_img)//2], None, 0,255, cv2.NORM_MINMAX)
 print(np.max(dimg))
 cv2.imshow("a",dimg)
 while True:
